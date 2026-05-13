63d8f265375829bd3a9ff77dc7e9594b664f2a83
phillip <pche@cpp.edu>
2026-05-13 13:32:33 -0700

    Initial Commit

---
+++ Offbeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Offbeat
+//
+//  Created by Phillip Che on 5/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OffbeatTests/OffbeatTests.swift
@@ -0,0 +1,19 @@
+//
+//  OffbeatTests.swift
+//  OffbeatTests
+//
+//  Created by Phillip Che on 5/13/26.
+//
+
+import Testing
+@testable import Offbeat
+
+struct OffbeatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
